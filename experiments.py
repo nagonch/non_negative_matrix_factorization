164f3dab76d5e8f07f135603f658f2725f35340a
Your Name <you@example.com>
2023-09-29 14:03:07 +1000

    experiments

--- experiments.py
+++ experiments.py
@@ -2,6 +2,7 @@ from metrics import get_metrics
 from data import load_data
 from nmf import LeeSeungNMF
 import warnings
+import pandas as pd
 
 warnings.filterwarnings("ignore")
 
@@ -22,8 +23,44 @@ if __name__ == "__main__":
     algorithms = [LeeSeungNMF]
     noise_types = [None, "salt_and_pepper", "occlusion"]
     datasets = ["ORL", "CroppedYaleB"]
+    data = []
+    index = []
     for alg in algorithms:
+        alg_name = alg.__name__
         for noise_type in noise_types:
             for dataset in datasets:
-                print(noise_type, dataset)
-                print(run_experiment(dataset, alg, noise_type))
+                print(f"Evaluating {alg_name}, {dataset}, {noise_type}...")
+                metrics = run_experiment(dataset, alg, noise_type)
+                index.append(
+                    [
+                        alg_name,
+                        noise_type if noise_type is not None else "-",
+                        dataset,
+                    ]
+                )
+                data.append(
+                    [
+                        metrics.nmi,
+                        metrics.rmse,
+                        metrics.accuracy,
+                    ]
+                )
+    index = pd.MultiIndex.from_tuples(
+        index,
+        names=[
+            "algorithm",
+            "noise_type",
+            "dataset",
+        ],
+    )
+    data = pd.DataFrame(
+        data,
+        columns=[
+            "nmi",
+            "rmse",
+            "accuracy",
+        ],
+        index=index,
+    )
+    print("\n")
+    print(data)
