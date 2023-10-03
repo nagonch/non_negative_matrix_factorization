5c049c6222d65a3c68c7d1d5c2afb2321a5da24b
Your Name <you@example.com>
2023-10-03 21:05:33 +1100

    add fixed experiments

--- experiments.py
+++ experiments.py
@@ -1,4 +1,4 @@
-from metrics import get_metrics, get_k_means_metrics
+from metrics import get_metrics, get_k_means_metrics, NMFMetrics
 from data import load_data
 from nmf import LeeSeungNMF, RobustNMF, RobustL1NMF
 import warnings
@@ -10,25 +10,49 @@ import numpy.typing as npt
 warnings.filterwarnings("ignore")
 
 
-def run_experiment(dataset: npt.NDArray[np.uint8], method, noise_type: str):
-    images, labels = load_data(
+def run_experiment(
+    dataset: npt.NDArray[np.uint8],
+    method,
+    noise_type: str,
+    num_iterations=5,
+    random_fraction=0.85,
+):
+    images_data, labels_data = load_data(
         root=f"data/{dataset}", corruption_type=noise_type
     )
-    if method == "KMEANS":
-        metrics = get_k_means_metrics(images, labels)
-    else:
-        K = len(set(labels))
-        nmf_method = method(K)
-        W, H, S = nmf_method.fit(images)
-        metrics = get_metrics(images, W, H, S, labels)
+    n_items = int(labels_data.shape[0] * random_fraction)
+    inds = np.arange(0, labels_data.shape[0], 1)
+    metrics_array = []
+    for _ in range(num_iterations):
+        np.random.shuffle(inds)
+        images = images_data[:, inds][:, :n_items]
+        labels = labels_data[inds][:n_items]
+        if method == "KMEANS":
+            metrics = get_k_means_metrics(images, labels)
+        else:
+            K = len(set(labels))
+            nmf_method = method(K)
+            W, H, S = nmf_method.fit(images)
+            metrics = get_metrics(images, W, H, S, labels)
+        metrics_array.append(metrics)
 
-    return metrics
+    result_metrics = NMFMetrics(0, 0, 0)
+    result_metrics.rmse = np.mean([metric.rmse for metric in metrics_array])
+    result_metrics.nmi = np.mean([metric.nmi for metric in metrics_array])
+    result_metrics.accuracy = np.mean([metric.accuracy for metric in metrics_array])
+
+    return result_metrics
 
 
-def all_experiments():
+def all_experiments(
+    iterations_per_eval: int = 5,
+    sampling_fraction: float = 0.85,
+):
     algorithms = [LeeSeungNMF, RobustNMF, RobustL1NMF, "KMEANS"]
     noise_types = [None, "salt_and_pepper", "occlusion"]
-    datasets = ["ORL", "CroppedYaleB"]
+    datasets = [
+        "ORL",
+    ]  # "CroppedYaleB"]
     data = []
     index = []
     for alg in algorithms:
@@ -38,7 +62,9 @@ def all_experiments():
                 print(
                     f"Evaluating {alg_name}, {dataset}, {noise_type if not noise_type is None else 'no noise'}..."
                 )
-                metrics = run_experiment(dataset, alg, noise_type)
+                metrics = run_experiment(
+                    dataset, alg, noise_type, iterations_per_eval, sampling_fraction
+                )
                 index.append(
                     [
                         alg_name,
@@ -84,14 +110,20 @@ def get_results(
     print(data)
 
 
-def main(save_results: bool = False):
-    data, index = all_experiments()
+def main(
+    save_results: bool = False,
+    iterations_per_eval: int = 5,
+    sampling_fraction: float = 0.85,
+):
+    data, index = all_experiments(iterations_per_eval, sampling_fraction)
     get_results(data, index, save_csv=save_results)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--save-csv", action="store_true")
+    parser.add_argument("--iterations-per-eval", type=int, default=5)
+    parser.add_argument("--sampling-fraction", type=float, default=0.85)
 
     args = parser.parse_args()
-    main(args.save_csv)
+    main(args.save_csv, args.iterations_per_eval, args.sampling_fraction)
